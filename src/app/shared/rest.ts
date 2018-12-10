9588da298761e4a8668d8cadb6f5637e7ce361d0
CANER GOEZUEBUEYUEK <goca1011@HS-Karlsruhe.de>
2018-12-10 18:21:49 +0100

    Finished Login
    F-IN LIT !!!

--- src/app/shared/rest.ts
+++ src/app/shared/rest.ts
@@ -9,3 +9,5 @@ const BASE_PATH = '/rest'
 export const BASE_URI = `${SCHEME}://${SERVERNAME}:${PORT}${BASE_PATH}`
 
 export const HOME_PATH = 'home'
+export const DETAILS_KUNDEN_PATH = 'details'
+export const KUNDEN_PATH = 'kunden'
