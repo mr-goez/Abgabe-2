9f5e3a065fc0997c7bb447c3f177bb0e861b2ba5
JONAS LEPKA <lejo1026 (at) HS-Karlsruhe.de>
2018-12-16 12:41:07 +0100

    Update rest.ts

--- src/app/shared/rest.ts
+++ src/app/shared/rest.ts
@@ -10,4 +10,4 @@ export const BASE_URI = `${SCHEME}://${SERVERNAME}:${PORT}${BASE_PATH}`
 
 export const HOME_PATH = 'home'
 export const DETAILS_KUNDEN_PATH = 'details'
-export const KUNDEN_PATH = 'kunden'
+export const KUNDEN_PATH = ''
