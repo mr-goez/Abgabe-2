911f54bdde69f0b35443e268e98a66dc3c3521c8
JONAS LEPKA <lejo1026 (at) HS-Karlsruhe.de>
2018-12-19 17:34:23 +0100

    BugFixes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,8 @@
 import { NgModule, Type } from '@angular/core'
 import { Title } from '@angular/platform-browser'
 
+import { SharedModule } from '../shared/shared.module'
+
 import { HomeComponent } from './home.component'
 
 const komponentenExport: Array<Type<any>> = [HomeComponent]
@@ -8,6 +10,7 @@ const komponentenIntern: Array<Type<any>> = []
 
 @NgModule({
     // Der Singleton-Service "Title" wird benoetigt
+    imports: [SharedModule.forRoot()],
     declarations: [...komponentenExport, ...komponentenIntern],
     providers: [Title],
     exports: komponentenExport,
