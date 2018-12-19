cad52529af300475547bb3f9c3e6db07932df6e6
JONAS LEPKA <lejo1026 (at) HS-Karlsruhe.de>
2018-12-19 17:57:33 +0100

    polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,7 +18,7 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+// ** IE9, IE10 and IE11 requires all of the following polyfills. **/
 // import 'core-js/es6/symbol';
 // import 'core-js/es6/object';
 // import 'core-js/es6/function';
@@ -38,7 +38,7 @@
  * Google bot use ES5.
  * FYI: Googlebot uses a renderer following the similar spec to Chrome 41.
  * https://developers.google.com/search/docs/guides/rendering
- **/
+ */
 // import 'core-js/es6/array';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
@@ -51,7 +51,7 @@
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- **/
+ */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
